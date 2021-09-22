e515fe406fed4536d16fdc0a80db8110835d1541
Daniela Diaz Bejarano <60675531+DanielaDiazBej@users.noreply.github.com>
2021-09-22 12:24:40 -0500

    p stilos new2

--- js/app.js
+++ js/app.js
@@ -61,7 +61,7 @@ function cameraStarted(){
     $('body').css('overflow-y','hidden');
 }
 
-/* function cameraStopped(){
+function cameraStopped(){
     $("#errorMsg").addClass("d-none");
     $("#wpfront-scroll-top-container").removeClass("d-none");
     $("#webcam-control").removeClass("webcam-on");
@@ -70,7 +70,7 @@ function cameraStarted(){
     $(".webcam-container").addClass("d-none");
     $("#button-start-cam").html("Click to Start Camera");
     $('.md-modal').removeClass('md-show');
-} */
+} 
 
 
 $("#take-photo").click(function () {
